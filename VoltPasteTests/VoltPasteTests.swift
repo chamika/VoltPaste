895f27423e436ccb73c060d4a8be6cd2d04d94e5
Chamika <chamikaw@gmail.com>
2026-02-27 08:50:22 +0000

    Initial Commit

---
+++ VoltPasteTests/VoltPasteTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoltPasteTests.swift
+//  VoltPasteTests
+//
+//  Created by Chamika Weerasinghe on 27/02/2026.
+//
+
+import Testing
+@testable import VoltPaste
+
+struct VoltPasteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
